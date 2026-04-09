e8e7fa7f0131a1576615d18b931bd4130ab98a67
gitGhost3Am <xvictorgg877@gmail.com>
2026-04-09 12:51:37 -0300

    Ajuste de elementos na página. Adição de carrosel na primeira imagem.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const tabs = document.querySelectorAll(".tab-btn");
+// const tabs = document.querySelectorAll(".tab-btn");
 
-tabs.forEach((tab) => tab.addEventListener("click", () => tabClicked(tab)));
+// tabs.forEach((tab) => tab.addEventListener("click", () => tabClicked(tab)));
 
-const tabClicked = (tab) => {
-  tabs.forEach((tab) => tab.classList.remove("active"));
-  tab.classList.add("active");
+// const tabClicked = (tab) => {
+//   tabs.forEach((tab) => tab.classList.remove("active"));
+//   tab.classList.add("active");
 
-  const contents = document.querySelectorAll(".content");
-  contents.forEach((content) => content.classList.remove("show"));
+//   const contents = document.querySelectorAll(".content");
+//   contents.forEach((content) => content.classList.remove("show"));
 
-  const contentId = tab.getAttribute("content-id");
-  const content = document.getElementById(contentId);
+//   const contentId = tab.getAttribute("content-id");
+//   const content = document.getElementById(contentId);
 
-  content.classList.add("show");
-};
+//   content.classList.add("show");
+// };
 
-const currentActiveTab = document.querySelector(".tab-btn.active");
-tabClicked(currentActiveTab);
+// const currentActiveTab = document.querySelector(".tab-btn.active");
+// tabClicked(currentActiveTab);
+
+
+
+//
+const img_carrosel = document.querySelectorAll('.slides img');
+const slides = document.querySelector('.slides');
+const btn_carrosel = document.querySelector('#btn-carrosel');
+let slideAtual = 0
+btn_carrosel.addEventListener('click', ()=>{
+  console.log('cliquei');
+    slideAtual++;
+    if (slideAtual >= img_carrosel.length){
+      slideAtual = 0; 
+    };
+    let altCarrosel = -slideAtual * 100;
+    slides.style.transform = `translateX(${altCarrosel}%)`;   
+})
